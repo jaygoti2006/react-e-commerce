6ead5248ea51da95bc5d9b37b7cde71c2e6c1591
jaygoti2006 <jaygoti2006@gmail.com>
2026-07-04 17:54:39 +0530

    add checkout page

--- front-end/src/pages/checkout/Checkout.jsx
+++ front-end/src/pages/checkout/Checkout.jsx
@@ -1,9 +1,22 @@
-import Header from '../../Header';
+import CheckoutHeader from './CheckoutHeader';
+import CheckoutItem from './CheckoutItem';
 
-export default function Checkout(){
+
+export default function Checkout() {
     return (
         <>
-            <Header />
+            <CheckoutHeader />
+            <div className="lg:max-w-6xl mx-auto px-3 py-15">
+                <h2 className="text-[22px] font-bold mb-4">Review your order</h2>
+                <div className="flex gap-2">
+                    <div className="grow">
+                        <CheckoutItem />
+                    </div>
+                    <div className="w-87.5">
+
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
